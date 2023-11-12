925923df23be60c5844b6dd3d943c37b70772104
Emre Karaosmanoglu <emrekaraos@outlook.com>
2023-11-12 21:29:22 +0300

    Add external file and add nlohmann/json project

--- app/main.cc
+++ app/main.cc
@@ -1,14 +1,20 @@
 #include <iostream>
+
+#include <nlohmann/json.hpp>
+
 #include "config.hpp"
 #include "my_lib.h"
 
-int main(){
-
+int main()
+{
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
 
-    print_hello_world();
-    
-    return 0;
+    std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "."
+              << NLOHMANN_JSON_VERSION_MINOR << "."
+              << NLOHMANN_JSON_VERSION_PATCH << "\n";
 
+    print_hello_world();
+
+    return 0;
 }
